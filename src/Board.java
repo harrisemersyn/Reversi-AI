6aec06ebe9da9bc7782bfe1a601cac0c0c34ac1b
Emersyn Harris <eharr21@u.rochester.edu>
2022-04-20 13:17:04 -0400

    methods for checking pieces on the board

--- src/Board.java
+++ src/Board.java
@@ -1,26 +1,79 @@
 public class Board{
 
-    enum Color{
+    enum Piece{
         WHITE,
-        BLACK
+        BLACK,
+        EMPTY
     }
 
-    public Color[][] board;
+    public Piece[][] board;
 
     public Board(){
-        this.board = new Color[8][8];
+        this.board = new Piece[8][8];
         //Sets pieces in the middle of the board
-        this.board[3][3] = WHITE;
-        this.board[4][4] = WHITE;
-        this.board[3][4] = BLACK;
-        this.board[4][3]= BLACK;
+        this.board[3][3] = Piece.WHITE;
+        this.board[4][4] = Piece.WHITE;
+        this.board[3][4] = Piece.BLACK;
+        this.board[4][3]= Piece.BLACK;
     }
 
-    //Checks to see if a move would be legal
-    public boolean CheckLegality(int row, int column){
+    /*
+    Checks for the piece color (or empty if no piece present) at a specific direction around a given piece
+    */
 
-        //in progress
-
+    public Piece checkLeft(int row, int column){
+        if(column - 1 < 0){
+            return null;
+        }
+        return(this.board[row][column-1]);
     }
 
+    public Piece checkRight(int row, int column){
+        if(column + 1 > 7){
+            return null;
+        }
+        return(this.board[row][column+1]);
+    }
+
+    public Piece checkTop(int row, int column){
+        if(row - 1 < 0){
+            return null;
+        }
+        return(this.board[row-1][column]);
+    }
+
+    public Piece checkBottom(int row, int column){
+        if(row + 1 > 7){
+            return null;
+        }
+        return(this.board[row+1][column]);
+    }
+
+    public Piece checkDiagTopLeft(int row, int column){
+        if(row - 1 < 0 || column - 1 < 0){
+            return null;
+        }
+        return(this.board[row-1][column-1]);
+    }
+
+    public Piece checkDiagTopRight(int row, int column){
+        if(row - 1 < 0 || column + 1 > 7){
+            return null;
+        }
+        return(this.board[row-1][column+1]);
+    }
+
+    public Piece checkDiagBottomLeft(int row, int column){
+        if(row + 1 > 7 || column - 1 < 0){
+            return null;
+        }
+        return(this.board[row+1][column-1]);
+    }
+
+    public Piece checkDiagBottomRight(int row, int column){
+        if(row + 1 > 7 || column + 1 > 7){
+            return null;
+        }
+        return(this.board[row+1][column+1]);
+    }
 }
